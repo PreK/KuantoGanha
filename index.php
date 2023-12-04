6631320ffc300c65e3c3910db5796c52ff4d0917
prek <prekpower@gmail.com>
2023-12-04 18:49:50 +0000

    Adicionado modulo sidebar.php

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Analyzer Dashboard</title>
+    <title>KuantoGanha.pt</title>
     <link href="css/bootstrap.min.css" rel="stylesheet"> <!-- Link para o Bootstrap -->
 </head>
 <body>
 <div class="container-fluid">
     <div class="row flex-nowrap">
         <!-- Inclui a barra lateral -->
-        <?php include 'sidebar.php'; ?>
+        <?php include 'modules/sidebar.php'; ?>
 
         <div class="col py-3">
             <!-- Conteúdo principal -->
-            <h3>Welcome to Team Analyzer</h3>
+            <h3>Benvindo ao KuantoGanha.pt</h3>
             <p>Here you can view various analytics about your team.</p>
 
             <!-- Conteúdo do gráfico será inserido aqui -->
