ba7c1caf4e0a4daab5985acb68f035a12b1c07de
prek <prekpower@gmail.com>
2023-12-05 19:34:18 +0000

    add login and logout

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+// Start the session (if not already started)
+session_start();
+
+// Check if the user is logged in
+$logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- modules/sidebar.php
+++ modules/sidebar.php
@@ -1,11 +1,17 @@
 <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
     <div class="position-sticky">
         <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">
-                    Página 1
-                </a>
-            </li>
+            <?php if ($logged_in) { ?>
+                <!-- If the user is logged in, display the logout link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+            <?php } else { ?>
+                <!-- If the user is not logged in, display the login link -->
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+            <?php } ?>
             <li class="nav-item">
                 <a class="nav-link" href="#">
                     Página 2
