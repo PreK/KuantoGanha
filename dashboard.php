5541b96abca492fb8328c12a73cb9e1bcb9e2a74
prek <prekpower@gmail.com>
2023-12-05 19:55:10 +0000

    Include sidebar on dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,6 @@ session_start();
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("Location: login.php");
 }
-include 'sidebar.php';
+include('modules/sidebar.php');
 ?>
 <h1>Welcome to the Dashboard</h1>
