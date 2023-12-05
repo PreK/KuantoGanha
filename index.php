2173ad32d255d2cdf82c3f2672c79e0501f3a543
prek <prekpower@gmail.com>
2023-12-05 19:25:40 +0000

    Fix modules/sidebar.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <div class="container-fluid">
     <div class="row">
         <!-- Barra lateral -->
-        <?php include('sidebar.php'); ?>
+        <?php include('modules/sidebar.php'); ?>
 
         <!-- Conteúdo principal -->
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
