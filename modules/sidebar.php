d9bd3a974625c49111083e715339c8f44a3a8485
prek <prekpower@gmail.com>
2023-12-05 20:47:29 +0000

    logout.php

--- modules/sidebar.php
+++ modules/sidebar.php
@@ -2,7 +2,6 @@
     <div class="position-sticky">
         <ul class="nav flex-column">
             <?php
-            session_start();
             // Check if the user is logged in
             if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){?>
                 <!-- If the user is logged in, display the logout link -->
