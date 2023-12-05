ac33c924472e7a2a1ed60efa6cdd3a559fce7b5a
prek <prekpower@gmail.com>
2023-12-05 19:39:36 +0000

    updated welcome.php

--- welcome.php
+++ welcome.php
@@ -1,6 +1,13 @@
 <?php
-session_start();
+session_start(); // Make sure to start the session
 
-if(!isset($_SESSION["loggedin"]) || $_SESSION)
-
+// Check if the user is logged in
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+    // User is logged in, redirect to index.php
+    header('Location: index.php');
+    exit(); // Make sure to terminate the script after the redirection
+} else {
+    // If the user is not logged in, display an appropriate error message or action
+    echo "You are not logged in. <a href='login.php'>Log in</a>";
+}
 ?>
