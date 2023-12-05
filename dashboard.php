341d6ab4d107da2aadf74112d1e4f85ad10e69d0
prek <prekpower@gmail.com>
2023-12-05 19:54:00 +0000

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['user'])) {
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("Location: login.php");
 }
 include 'sidebar.php';
