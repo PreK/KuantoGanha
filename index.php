c680f1d262794add02f9528c79ec3e4c29ce2969
prek <prekpower@gmail.com>
2023-12-05 19:22:26 +0000

    Fix da barra lateral

--- index.php
+++ index.php
@@ -3,25 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>KuantoGanha.pt</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet"> <!-- Link para o Bootstrap -->
 </head>
 <body>
 <div class="container-fluid">
-    <div class="row flex-nowrap">
-        <!-- Inclui a barra lateral -->
-        <?php include 'modules/sidebar.php'; ?>
+    <div class="row">
+        <!-- Barra lateral -->
+        <?php include('sidebar.php'); ?>
 
-        <div class="col py-3">
-            <!-- Conteúdo principal -->
-            <h3>Bem-vindo ao KuntoGanha</h3>
-            <p>Bla bla wiskas.</p>
-
-            <!-- Conteúdo do gráfico será inserido aqui -->
-        </div>
+        <!-- Conteúdo principal -->
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <!-- Seu conteúdo principal aqui -->
+            <h1>Página Principal</h1>
+            <p>Bem-vindo à página principal da sua aplicação.</p>
+        </main>
     </div>
 </div>
 
-<script src="js/bootstrap.min.js"></script> <!-- Script do Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
