fddfbc5dc9cb832598dfb4bf35c6e66e2dd0c834
Filipe Silva <prekpower@gmail.com>
2023-12-06 21:13:29 +0000

    Register more pretty

--- modules/register.php
+++ modules/register.php
@@ -98,53 +98,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     unset($pdo);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/sidebar.css">
-    <title>Registar</title>
-</head>
-<body>
-<div class="container-fluid">
-
-    <div class="row">
-        <!-- Sidebar -->
-        <?php include('modules/sidebar.php'); ?>
-<div>
-    <h2>Register</h2>
-    <p>Please fill this form to create an account.</p>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div>
-            <label>Username</label>
-            <input type="text" name="username" value="<?php echo $username; ?>">
-            <span><?php echo $username_err; ?></span>
-        </div>
-        <div>
-            <label>Email</label>
-            <input type="email" name="email" value="<?php echo $email; ?>">
-            <span><?php echo $email_err; ?></span>
-        </div>
-        <div>
-            <label>Password</label>
-            <input type="password" name="password">
-            <span><?php echo $password_err; ?></span>
-        </div>
-        <div>
-            <label>Confirm Password</label>
-            <input type="password" name="confirm_password">
-            <span><?php echo $confirm_password_err; ?></span>
-        </div>
-        <div>
-            <input type="submit" value="Submit">
-        </div>
-    </form>
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="register-form">
+        <h2>Register</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="mb-3">
+                <label class="form-label">Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="text-danger"><?php echo $username_err; ?></span>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
+                <span class="text-danger"><?php echo $email_err; ?></span>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="text-danger"><?php echo $password_err; ?></span>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control">
+                <span class="text-danger"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div>
+                <input type="submit" class="btn btn-primary" value="Submit">
+            </div>
+        </form>
     </div>
-</div>
-</body>
-<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"></script>
-<script src="https://unpkg.com/@popperjs/core@2"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</html>
+</main>
