edd76c240dcbee6295d4520adfe6bb74cc04b002
Filipe Silva <prekpower@gmail.com>
2023-12-22 20:02:20 +0000

    jobs.php first fix

--- js/ajax.js
+++ js/ajax.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
+    // Vincula eventos de clique para carregar páginas específicas
     $('#loginLink, #registerLink, #jobsLink').click(function(event) {
         event.preventDefault();
         var page = $(this).attr('id').replace('Link', '') + '.php';
         loadContent(page);
     });
 
+    // Carrega o conteúdo dinamicamente e atualiza mainContent
     function loadContent(page) {
         $.get('modules/' + page, function(html) {
             $('#mainContent').html(html);
             bindFormSubmit();
+            bindMenuLinks();
         }).fail(function() {
             $('#mainContent').html('Erro ao carregar a página');
         });
     }
 
+    // Vincula eventos de submissão de formulários
     function bindFormSubmit() {
         $('#mainContent').find('form').submit(function(event) {
             event.preventDefault();
@@ -21,21 +25,31 @@ $(document).ready(function() {
         });
     }
 
+    // Trata a submissão do formulário
     function submitForm($form) {
         var url = $form.attr('action') || 'modules/' + $form.attr('class').split('-')[0] + '.php';
 
         $.post(url, $form.serialize(), function(html) {
             $('#mainContent').html(html);
             bindFormSubmit();
+            bindMenuLinks();
 
-            // Delay na verificação para garantir que o DOM foi atualizado
-            setTimeout(function() {
-                if ($('#mainContent').text().includes('success')) {
-                    window.location.reload();
-                }
-            }, 100); // Ajuste o tempo conforme necessário
+            if (html.includes('success')) {
+                window.location.reload();
+            }
         }).fail(function() {
             $('#mainContent').html('Erro ao processar o formulário');
+        });
+    }
+
+    // Vincula eventos de clique aos links dentro de mainContent
+    function bindMenuLinks() {
+        $('#mainContent a').click(function(event) {
+            event.preventDefault();
+            var href = $(this).attr('href');
+            if (href) {
+                loadContent(href);
+            }
         });
     }
 });
