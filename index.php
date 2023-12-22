b170c384ab8aba868bbab3a7fdcccea5fc6282df
Filipe Silva <prekpower@gmail.com>
2023-12-22 19:49:17 +0000

    jobs.php first fix

--- index.php
+++ index.php
@@ -253,5 +253,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script><script src="dashboard.js"></script>
     <script src="js/ajax.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
   </body>
 </html>
--- js/ajax.js
+++ js/ajax.js
@@ -1,85 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loginLink = document.getElementById('loginLink');
-    var registerLink = document.getElementById('registerLink');
-    var jobsLink = document.getElementById('jobsLink');
-
-    if (loginLink) {
-        loginLink.addEventListener('click', function() {
-            loadContent('login.php');
-        });
-    }
-    if (registerLink) {
-        registerLink.addEventListener('click', function() {
-            loadContent('register.php');
-        });
-    }
-    if (jobsLink) {
-        jobsLink.addEventListener('click', function() {
-            loadContent('jobs.php');
-        });
-    }
-    bindFormSubmit();
-});
-
-function loadContent(page) {
-    fetch('modules/' + page)
-        .then(response => {
-            if (response.ok) {
-                return response.text();
-            } else {
-                throw new Error('Falha ao carregar conteúdo: ' + response.status);
-            }
-        })
-        .then(html => {
-            document.getElementById('mainContent').innerHTML = html;
-            bindFormSubmit();
-        })
-        .catch(error => {
-            console.error('Erro ao carregar conteúdo:', error);
-            document.getElementById('mainContent').innerHTML = 'Erro ao carregar a página';
-        });
-}
-
-function bindFormSubmit() {
-    document.querySelectorAll('form').forEach(function(form) {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            var actionUrl = form.getAttribute('action');
-            submitForm(form, actionUrl);
-        });
+$(document).ready(function() {
+    // Carregar conteúdo para login, registro, e jobs
+    $('#loginLink, #registerLink, #jobsLink').click(function(event) {
+        event.preventDefault();
+        var page = $(this).attr('id').replace('Link', '') + '.php';
+        loadContent(page);
     });
-}
 
-function submitForm(form, url) {
-    if (!url || url === '') {
-        if (form.classList.contains('login-form')) {
-            url = 'modules/login.php';
-        } else if (form.classList.contains('register-form')) {
-            url = 'modules/register.php';
-        } else if (form.classList.contains('jobs-form')) {
-            url = 'modules/jobs.php';
-        } else {
-            console.error('Formulário desconhecido, não é possível determinar o endpoint.');
-            return;
-        }
+    function loadContent(page) {
+        $.get('modules/' + page, function(html) {
+            $('#mainContent').html(html);
+            bindFormSubmit();
+        }).fail(function() {
+            $('#mainContent').html('Erro ao carregar a página');
+        });
     }
 
-    const formData = new FormData(form);
-    fetch(url, {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.text())
-        .then(html => {
-            if (html.includes('success')) {
-                window.location.reload(); // Recarregar a página se a resposta contém 'success'
-            } else {
-                document.getElementById('mainContent').innerHTML = html;
-                bindFormSubmit(); // Re-bind para novos formulários
-            }
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-            document.getElementById('mainContent').innerHTML = 'Erro ao processar o formulário';
+    function bindFormSubmit() {
+        $('form').submit(function(event) {
+            event.preventDefault();
+            submitForm($(this));
         });
-}
+    }
+
+    function submitForm($form) {
+        var url = $form.attr('action') || 'modules/' + $form.attr('class').split('-')[0] + '.php';
+
+        $.post(url, $form.serialize(), function(html) {
+            $('#mainContent').html(html);
+            bindFormSubmit();
+            if (html.includes('success')) {
+                window.location.reload();
+            }
+        }).fail(function() {
+            $('#mainContent').html('Erro ao processar o formulário');
+        });
+    }
+});
