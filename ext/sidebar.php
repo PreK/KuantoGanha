658f10cd0b5aa61d5b3f4304baeec616390642e3
Filipe Silva <prekpower@gmail.com>
2023-12-22 15:48:09 +0000

    sidebar jobs update

--- ext/sidebar.php
+++ ext/sidebar.php
@@ -11,13 +11,21 @@ session_start();
     <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
         <ul class="nav flex-column">
 <!--            Menu with Login and Register if logged out-->
-            <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){?>
+            <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2" href="#" id="profileLink">
                         <i class="bi bi-person"></i>
                         Perfil
                     </a>
                 </li>
+                <?php if ($_SESSION["username"] === "admin") { ?>
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center gap-2" href="modules/jobs.php" id="jobsLink">
+                            <i class="bi bi-journal-bookmark"></i>
+                            Profissões
+                        </a>
+                    </li>
+                <?php } ?>
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2" href="modules/logout.php">
                         <i class="bi bi-box-arrow-left"></i>
@@ -25,7 +33,7 @@ session_start();
                     </a>
                 </li>
             <?php } else { ?>
-                <!-- Usuário Não Logado: Mostrar Login e Registro -->
+                <!-- Utilizador não Logado: Mostrar Login e Registro -->
                 <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#" id="loginLink">
                         <i class="bi bi-box-arrow-in-right"></i>
@@ -39,9 +47,9 @@ session_start();
                     </a>
                 </li>
             <?php } ?>
-        </ul>
 
-        <hr class="my-3">
+
+            <hr class="my-3">
 
         <!-- Outras Opções de Menu -->
         <ul class="nav flex-column mb-auto">
