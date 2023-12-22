5e86cb54c3f1ecd8bec3f1542503430643da5af9
Filipe Silva <prekpower@gmail.com>
2023-12-22 16:07:22 +0000

    jobs.php first fix

--- modules/jobs.php
+++ modules/jobs.php
@@ -1,82 +1,90 @@
 <?php
 session_start();
+
 require_once 'dbconfig.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Verificar se o usuário é administrador
 if (!isset($_SESSION['loggedin']) || $_SESSION['username'] !== 'admin') {
-    header('Location: login.php'); // Redirecionar para a página de login se não for admin
+    header('Location: login.php');
     exit;
 }
 
 $pdo = getDbConnection();
+$job_err = "";
 
-// Lógica para adicionar um novo emprego
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_job'])) {
-    $title = trim($_POST['job_title']);
-    if (!empty($title)) {
-        $sql = "INSERT INTO jobs (title) VALUES (:title)";
+// Processamento de dados do formulário quando enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Adicionar emprego
+    if (isset($_POST["add_job"])) {
+        if (empty(trim($_POST["job_title"]))) {
+            $job_err = "Por favor, insira o título do emprego.";
+        } else {
+            $sql = "INSERT INTO jobs (title) VALUES (:title)";
+            if ($stmt = $pdo->prepare($sql)) {
+                $stmt->bindParam(":title", $param_title, PDO::PARAM_STR);
+                $param_title = trim($_POST["job_title"]);
+                $stmt->execute();
+                unset($stmt);
+            }
+        }
+    }
+
+    // Remover emprego
+    if (isset($_POST["remove_job"]) && !empty(trim($_POST["job_id"]))) {
+        $sql = "DELETE FROM jobs WHERE id = :job_id";
         if ($stmt = $pdo->prepare($sql)) {
-            $stmt->bindParam(":title", $title, PDO::PARAM_STR);
+            $stmt->bindParam(":job_id", $param_job_id, PDO::PARAM_INT);
+            $param_job_id = trim($_POST["job_id"]);
             $stmt->execute();
-            // Mensagem de sucesso ou redirecionamento pode ser adicionado aqui
+            unset($stmt);
         }
     }
 }
-
-// Lógica para remover um emprego
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['remove_job'])) {
-    $jobId = trim($_POST['job_id']);
-    $sql = "DELETE FROM jobs WHERE id = :job_id";
-    if ($stmt = $pdo->prepare($sql)) {
-        $stmt->bindParam(":job_id", $jobId, PDO::PARAM_INT);
-        $stmt->execute();
-        // Mensagem de sucesso ou redirecionamento pode ser adicionado aqui
-    }
-}
+unset($pdo);
 ?>
 
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <title>Administração de Empregos</title>
-    <!-- Aqui podem ser adicionados links para CSS, Bootstrap, etc. -->
-</head>
-<body>
-<div class="container">
-    <h2>Administração de Empregos</h2>
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="job-management-form">
+        <h2>Gestão de Profissões</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="mb-3">
+                <label class="form-label">Título do Emprego</label>
+                <input type="text" name="job_title" class="form-control">
+                <span class="text-danger"><?php echo $job_err; ?></span>
+            </div>
+            <div>
+                <input type="submit" name="add_job" class="btn btn-primary" value="Adicionar Emprego">
+            </div>
+        </form>
 
-    <!-- Formulário para adicionar um novo emprego -->
-    <form method="post">
-        <input type="text" name="job_title" placeholder="Título do Emprego">
-        <button type="submit" name="add_job">Adicionar Emprego</button>
-    </form>
+        <!-- Lista de empregos com opção de remoção -->
+        <table>
+            <tr>
+                <th>Profissão</th>
+                <th>Apagar</th>
+            </tr>
+            <?php
+            $pdo = getDbConnection();
+            $sql = "SELECT * FROM jobs";
+            foreach ($pdo->query($sql) as $row) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($row['title']) . "</td>";
+                echo "<td>
+                        <form method='post'>
+                            <input type='hidden' name='job_id' value='" . $row['id'] . "'>
+                            <button type='submit' name='remove_job' class='btn btn-danger'>Remover</button>
+                        </form>
+                      </td>";
+                echo "</tr>";
+            }
+            unset($pdo);
+            ?>
+        </table>
+    </div>
+</main>
 
-    <!-- Tabela para listar e remover empregos -->
-    <table>
-        <thead>
-        <tr>
-            <th>Título</th>
-            <th>Ações</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        $sql = "SELECT * FROM jobs";
-        foreach ($pdo->query($sql) as $row) {
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($row['title']) . "</td>";
-            echo "<td>
-                            <form method='post'>
-                                <input type='hidden' name='job_id' value='" . $row['id'] . "'>
-                                <button type='submit' name='remove_job'>Remover</button>
-                            </form>
-                          </td>";
-            echo "</tr>";
-        }
-        ?>
-        </tbody>
-    </table>
-</div>
-</body>
-</html>
