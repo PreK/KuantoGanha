b0275665caa4817b16191ccd25f40e986ef04346
Filipe Silva <prekpower@gmail.com>
2023-12-26 17:37:22 +0000

    Remake ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,94 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Função para carregar conteúdo no mainContent
-    function loadContent(url) {
-        fetch(url)
-            .then(response => response.text())
-            .then(html => {
-                document.getElementById('mainContent').innerHTML = html;
-            })
-            .catch(error => {
-                console.error('Erro ao carregar o conteúdo:', error);
-            });
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    // Função genérica para lidar com a submissão de formulários
+    function handleFormSubmit(formId, url) {
+        var form = document.getElementById(formId);
+        if (form) {
+            form.addEventListener('submit', function(e) {
+                e.preventDefault();
+                var formData = new FormData(this);
 
-    // Manipuladores de clique para links da barra lateral
-    document.querySelectorAll('.sidebar .nav-link').forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            console.log('Link clicado:', this.getAttribute('href')); // Para depuração
-            const url = this.getAttribute('href');
-            if (url !== '#') {
-                loadContent(url);
-            }
-        });
-    });
-
-    // Função para enviar dados de formulários via AJAX
-    function sendForm(form, url) {
-        var formData = new FormData(form);
-        fetch(url, {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Processar a resposta
-                // Aqui você pode redirecionar o usuário ou atualizar a interface
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-            });
-    }
-
-    // Intercepta submissões de formulário de login e registro
-    document.querySelectorAll('.ajaxForm').forEach(form => {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            // Coleta os dados do formulário e converte em um objeto
-            var formData = new FormData(this);
-            var object = {};
-            formData.forEach((value, key) => { object[key] = value; })
-
-            // Converte os dados do formulário em JSON
-            var json = JSON.stringify(object);
-
-            // Faz a requisição AJAX
-            fetch(this.getAttribute('action'), {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: json
-            })
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data); // Processa a resposta
-                    window.location.href = 'index.php'; // Redireciona para a página inicial
+                fetch(url, {
+                    method: 'POST',
+                    body: formData
                 })
-                .catch(error => {
-                    console.error('Erro:', error);
-                });
-        });
-    });
-
-
-    // Logout
-    const logoutLink = document.getElementById('logoutLink');
-    if (logoutLink) {
-        logoutLink.addEventListener('click', function(event) {
-            event.preventDefault();
-            fetch('modules/logout.php', { method: 'POST' })
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    // Redirecionar para a página de login ou atualizar a interface
-                })
-                .catch(error => {
-                    console.error('Erro no logout:', error);
-                });
-        });
+                    .then(response => response.text())
+                    .then(data => {
+                        // Lidar com a resposta
+                        alert(data); // Exibe a resposta do servidor
+                    })
+                    .catch(error => console.error('Erro na requisição AJAX:', error));
+            });
+        }
     }
+
+    // Configuração para cada formulário
+    handleFormSubmit('registerForm', 'register.php');
+    handleFormSubmit('loginForm', 'login.php');
+    handleFormSubmit('jobsForm', 'jobs.php');
+    // Adicione mais conforme necessário
 
 });
