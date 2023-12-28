a4645dcabb59991e90aca296a73ff21aa4dec022
Filipe Silva <prekpower@gmail.com>
2023-12-28 19:13:47 +0000

    Implement getInfo.php.php

--- modules/commonFunctions.php
+++ modules/commonFunctions.php
@@ -26,4 +26,63 @@ function getModalities(): bool|array
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+function getTopProfessionsData($district): array|bool
+{
+    $pdo = getDbConnection();
+    try {
+        // Modifique a consulta SQL para lidar com o caso "all"
+        $sql = $district === 'all' ?
+            "SELECT j.title, AVG(s.gross_amount) as averageSalary
+             FROM jobs j
+             JOIN user_jobs uj ON j.id = uj.job_id
+             JOIN salaries s ON uj.id = s.user_job_id
+             GROUP BY j.title
+             ORDER BY averageSalary DESC
+             LIMIT 5" :
+            "SELECT j.title, AVG(s.gross_amount) as averageSalary
+             FROM jobs j
+             JOIN user_jobs uj ON j.id = uj.job_id
+             JOIN salaries s ON uj.id = s.user_job_id
+             JOIN locations l ON uj.location_id = l.id
+             WHERE l.district = :district
+             GROUP BY j.title
+             ORDER BY averageSalary DESC
+             LIMIT 5";
+
+        $stmt = $pdo->prepare($sql);
+
+        // Apenas ligar o parâmetro se não for "all"
+        if ($district !== 'all') {
+            $stmt->bindParam(':district', $district);
+        }
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log("Erro ao obter dados: " . $e->getMessage());
+        return ["error" => "Erro ao obter dados do gráfico"];
+    }
+}
+
+function getRecentProfessionsData(): array|bool
+{
+    $pdo = getDbConnection();
+    try {
+        $sql = "SELECT j.title, l.district, wm.description, uj.start_date, uj.end_date
+                FROM user_jobs uj
+                JOIN jobs j ON uj.job_id = j.id
+                JOIN locations l ON uj.location_id = l.id
+                JOIN work_modalities wm ON uj.modality_id = wm.id
+                ORDER BY uj.start_date DESC
+                LIMIT 20";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log("Erro ao obter dados: " . $e->getMessage());
+        return ["error" => "Erro ao obter dados da tabela"];
+    }
+}
+
 ?>
--- modules/getInfo.php
+++ modules/getInfo.php
@@ -1,79 +1,14 @@
 <?php
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config/dbconfig.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/modules/commonFunctions.php';
 
 header('Content-Type: application/json');
 
-$pdo = getDbConnection();
-
 if (isset($_GET['requestType'])) {
-    switch ($_GET['requestType']) {
-        case 'chart':
-            echo json_encode(getTopProfessionsData($pdo, $_GET['district'] ?? ''));
-            break;
-        case 'table':
-            echo json_encode(getRecentProfessionsData($pdo));
-            break;
-        default:
-            echo json_encode(["error" => "Tipo de solicitação inválido"]);
-    }
-}
-
-function getTopProfessionsData($pdo, $district) {
-    try {
-        // Modifique a consulta SQL para lidar com o caso "all"
-        $sql = $district === 'all' ?
-            "SELECT j.title, AVG(s.gross_amount) as averageSalary
-             FROM jobs j
-             JOIN user_jobs uj ON j.id = uj.job_id
-             JOIN salaries s ON uj.id = s.user_job_id
-             GROUP BY j.title
-             ORDER BY averageSalary DESC
-             LIMIT 5" :
-            "SELECT j.title, AVG(s.gross_amount) as averageSalary
-             FROM jobs j
-             JOIN user_jobs uj ON j.id = uj.job_id
-             JOIN salaries s ON uj.id = s.user_job_id
-             JOIN locations l ON uj.location_id = l.id
-             WHERE l.district = :district
-             GROUP BY j.title
-             ORDER BY averageSalary DESC
-             LIMIT 5";
-
-        $stmt = $pdo->prepare($sql);
-
-        // Apenas ligar o parâmetro se não for "all"
-        if ($district !== 'all') {
-            $stmt->bindParam(':district', $district);
-        }
-
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        error_log("Erro ao obter dados: " . $e->getMessage());
-        return ["error" => "Erro ao obter dados do gráfico"];
-    }
-}
-
-function getRecentProfessionsData($pdo) {
-    try {
-        $sql = "SELECT j.title, l.district, wm.description, uj.start_date, uj.end_date
-                FROM user_jobs uj
-                JOIN jobs j ON uj.job_id = j.id
-                JOIN locations l ON uj.location_id = l.id
-                JOIN work_modalities wm ON uj.modality_id = wm.id
-                ORDER BY uj.start_date DESC
-                LIMIT 20";
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        error_log("Erro ao obter dados: " . $e->getMessage());
-        return ["error" => "Erro ao obter dados da tabela"];
-    }
+    echo match ($_GET['requestType']) {
+        'chart' => json_encode(getTopProfessionsData($_GET['district'] ?? '')),
+        'table' => json_encode(getRecentProfessionsData()),
+        default => json_encode(["error" => "Tipo de solicitação inválido"]),
+    };
 }
 ?>
