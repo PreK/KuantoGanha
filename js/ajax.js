3a9fb1d5cc66fd6a54d23299f89b371cfc080300
Filipe Silva <prekpower@gmail.com>
2023-12-28 13:23:08 +0000

    Remake perfil.php

--- js/ajax.js
+++ js/ajax.js
@@ -52,7 +52,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     })
                 })
                     .then(response => response.json())
-                    .then(data => console.log(data))
                     .then(data => {
                         if(data.success) {
                             location.reload();
